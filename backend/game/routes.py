2ea2710db929b5be79ff63b9888ff3a3d8f906ea
Hunter Salazar <Business@Corgo.org>
2026-01-31 12:56:35 -0700

    Added movement to backend

--- backend/game/routes.py
+++ backend/game/routes.py
@@ -6,6 +6,30 @@ from world.helpers import player_view
 game_router = APIRouter()
 
 
-@game_router.post("/map")
-def map_route(user: User = Depends(get_user)) -> dict:
-    return {"map": player_view(user)}
+@game_router.post("/view")
+def view_route(user: User = Depends(get_user)) -> dict:
+    return {"view": player_view(user)}
+
+
+@game_router.post("/move_north")
+def move_north(user: User = Depends(get_user)) -> dict:
+    user.y += 1
+    return {"message": "Moved north", "view": player_view(user)}
+
+
+@game_router.post("/move_south")
+def move_south(user: User = Depends(get_user)) -> dict:
+    user.y -= 1
+    return {"message": "Moved south", "view": player_view(user)}
+
+
+@game_router.post("/move_east")
+def move_east(user: User = Depends(get_user)) -> dict:
+    user.x += 1
+    return {"message": "Moved east", "view": player_view(user)}
+
+
+@game_router.post("/move_west")
+def move_west(user: User = Depends(get_user)) -> dict:
+    user.x -= 1
+    return {"message": "Moved west", "view": player_view(user)}
