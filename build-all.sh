ae8649156c792864bbf3579aac96abd905316528
Hunter Salazar <Business@Corgo.org>
2026-01-30 20:02:42 -0700

    Init docker compose

--- build-all.sh
+++ build-all.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 IFS=$'\n\t'
+
+sudo docker compose down
+sudo docker compose up --build -d
--- build-front.sh
+++ build-front.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
-#set -Eeuo pipefail
-#IFS=$'\n\t'
+set -Eeuo pipefail
+IFS=$'\n\t'
 
-cd frontend
-sudo docker stop ColorMask-frontend
-sudo docker rm ColorMask-frontend
-sudo docker build -t colormask-frontend .
-sudo docker run -d --name ColorMask-frontend -p 9001:80 colormask-frontend
+sudo docker compose build frontend
+sudo docker compose up -d frontend
