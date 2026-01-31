932637530a4c08e55ed437e210d0c7e8fe090560
Hunter Salazar <Business@Corgo.org>
2026-01-31 16:48:08 -0700

    Updated CORS & added theme_color to User backend

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,10 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://10.101.0.6",
+        "http://104.251.216.28",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
