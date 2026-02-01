7928e871d4a59fcc2739778a8392795dc09831da
Hunter Salazar <Business@Corgo.org>
2026-02-01 10:55:22 -0700

    Updated the color pallete and set the origin pink

--- backend/util/autos.py
+++ backend/util/autos.py
@@ -31,17 +31,26 @@ def startup() -> None:
         STATE.users[name] = gen_user(name)
         # STATE.themes[name] = ["#000000", "#FFFFFF"]
         STATE.themes[name] = [
-            "#f87171",
-            "#34d399",
-            "#60a5fa",
-            "#facc15",
-            "#a78bfa",
-            "#2dd4bf",
-            "#f472b6",
+            "#f2b3b0",
+            "#ffe08b",
+            "#aad4ff",
+            "#9ee0b9",
+            "#f0c1f0",
+            "#ffb8a3",
+            "#b0c6ff",
+            "#ffdca3",
+            "#c4f0a8",
+            "#a0e0ff",
+            "#f4d1c7",
+            "#d4baff",
+            "#c0fff0",
+            "#ffcae0",
+            "#e0ffb0",
         ]
 
     # Set a default token for Hunter (Quick Developmnent)
     STATE.auth_tokens["Hunter"] = "asd"
+    STATE.cells["0:0:0"] = "#FF69B4"
 
     # print(STATE.users)
     # print(STATE.auth_tokens)
